5a1b95b46e524815834e534a1b613843bc8418a7
Pierre-Yves Ricau <py.ricau@gmail.com>
2012-07-07 11:20:39 +0200

    adding missing Android classes

---
+++ src/android/app/Activity.java
@@ -0,0 +1,12 @@
+package android.app;
+
+import android.content.Context;
+import android.view.View;
+
+public class Activity extends Context {
+
+	public View findViewById(int id) {
+		return null;
+	}
+
+}
